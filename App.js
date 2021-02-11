cdc4d9b01c0bb2efa0b6c0d498a81fecd876b4ec
murilokrugner <murilokrugner@hotmail.com>
2021-02-11 17:49:34 -0300

    order by name

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text, ActivityIndicator} from 'react-native';
+
+import api from './src/services/api';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('users');
+
+      const first = response.data.slice(0, 5);
+
+      const order = first.map((item) => {
+        const teste = item.name;
+
+        return teste;
+      });
+
+      const orderBy = order.sort();
+
+      setUsers(orderBy);
+
+      setLoading(false);
+    }
+
+    loadUsers();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-    </View>
+    <>
+      {loading ? (
+        <View style={styles.container}>
+          <ActivityIndicator color="#000" size="large" />
+        </View>
+      ) : (
+        <View style={styles.container}>
+          {users.map((item) => (
+            <Text style={styles.text}>{item}</Text>
+          ))}
+        </View>
+      )}
+    </>
   );
 };
 
